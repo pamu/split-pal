9b67a94ac9f16fe3078c9f3b54741465faedfde4
pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-12-23 00:30:25 +0530

    contact utils

---
+++ src/main/scala/com/rxbytes/splitpal/utils/contacts/ContactsUtils.scala
@@ -0,0 +1,30 @@
+package com.rxbytes.splitpal.utils.contacts
+
+/**
+  * Created by pnagarjuna on 22/12/15.
+  */
+object ContactsUtils {
+
+  implicit class PhoneNumberUtils(val str: String) extends AnyVal {
+
+    def tenDigitNumber: Option[Long] = {
+      val trimmedStr = str.trim
+      val trimmedStrLen = trimmedStr.length
+      if (trimmedStrLen >= 10) {
+        val extra = trimmedStrLen - 10
+        val str10 = trimmedStr.substring(extra)
+        if (str10.matches("""[\d+]*""")) Some(str10.toLong) else None
+      } else None
+    }
+
+    def countryCode: CountryCode.CountryCode = CountryCode.IN
+  }
+
+}
+
+object CountryCode extends Enumeration {
+  type CountryCode = Value
+  val IN = Value("India")
+  val US = Value("America")
+  val UK = Value("England")
+}
