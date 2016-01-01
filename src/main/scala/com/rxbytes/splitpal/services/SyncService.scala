c0e7fc6596d9f17fa4e94d0f225e6a39710d317e
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-01-01 21:41:23 +0530

    sync service

---
+++ src/main/scala/com/rxbytes/splitpal/services/SyncService.scala
@@ -0,0 +1,14 @@
+package com.rxbytes.splitpal.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+  * Created by pnagarjuna on 01/01/16.
+  */
+class SyncService extends Service {
+
+  override def onBind(intent: Intent): IBinder = null
+
+}
