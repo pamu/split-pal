b80140b9259967e3544183cfa8b81c3cbad94c94
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-01-02 16:08:45 +0530

    sync service onstartcommand

--- src/main/scala/com/rxbytes/splitpal/services/SyncService.scala
+++ src/main/scala/com/rxbytes/splitpal/services/SyncService.scala
@@ -3,12 +3,20 @@ package com.rxbytes.splitpal.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 /**
   * Created by pnagarjuna on 01/01/16.
   */
 class SyncService extends Service {
 
+  private val LOG_TAG = classOf[SyncService].getSimpleName
+
   override def onBind(intent: Intent): IBinder = null
+
+  override def onStartCommand(intent: Intent, flags: Int, startId: Int): Int = {
+    Log.d(LOG_TAG, "onStartCommand SyncService")
+    super.onStartCommand(intent, flags, startId)
+  }
 
 }
