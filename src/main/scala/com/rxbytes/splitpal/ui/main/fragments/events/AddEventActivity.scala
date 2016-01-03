d9576d238214581608d89a7be4a36686671c5bfe
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-01-03 23:42:19 +0530

    add event

---
+++ src/main/scala/com/rxbytes/splitpal/ui/main/fragments/events/AddEventActivity.scala
@@ -0,0 +1,15 @@
+package com.rxbytes.splitpal.ui.main.fragments.events
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+  * Created by pnagarjuna on 02/01/16.
+  */
+class AddEventActivity extends AppCompatActivity {
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+  }
+
+}
